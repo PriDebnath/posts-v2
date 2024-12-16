41e5b5e8034fa079f978dc3c1bc1d5d55172f13b
Pritam <debnathpritam0802@gmail.com>
2024-12-16 16:29:25 +0530

    created  PostTag

--- backend/post_v2/apps/post/admin.py
+++ backend/post_v2/apps/post/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from post_v2.apps.post.models import Post, Tag
+from post_v2.apps.post.models import Post, Tag, PostTag
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
+admin.site.register(PostTag)
--- backend/post_v2/apps/post/models.py
+++ backend/post_v2/apps/post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="posts", null=True
+    )
 
     def __str__(self):
         return f"{self.id } - {self.title}"
@@ -16,3 +20,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return f"{self.id } - {self.title}"
+
+
+class PostTag(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ("post", "tag")
+
+    def __str__(self):
+        return f"{self.id } - Post: {self.post}  - Tag: {self.tag}"
