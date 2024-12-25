1f1042af786eab4f8995b1e1bcf2fe6bca168bfa
Pritam <debnathpritam0802@gmail.com>
2024-12-25 10:10:33 +0530

    added button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NzSpinModule,
+    NzButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideNzI18n(en_US),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
