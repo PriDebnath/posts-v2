c12c459f3b0f29400b0d68f6cddf0ac05088a380
Pritam <debnathpritam0802@gmail.com>
2025-01-24 14:16:31 +0530

    added all CRUD services

--- frontend/src/app/module/post/post.module.ts
+++ frontend/src/app/module/post/post.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostRoutingModule } from './post-routing.module';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { GetColorPipe } from './pipes/get-color/get-color.pipe';
+import { PostCardComponent } from './components/post-list/post-card/post-card.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [PostListComponent, GetColorPipe],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [PostListComponent, GetColorPipe, PostCardComponent],
   exports: [GetColorPipe],
-  imports: [CommonModule, PostRoutingModule],
+  imports: [CommonModule, PostRoutingModule, NzIconModule, FormsModule],
 })
 export class PostModule {}
